920d0bebdacc8bb91735fc3deec497cbce77ede4
Hiroyuki Hasebe <hiroyha@microsoft.com>
2020-07-24 00:39:31 +0900

    add health api

--- src/hello/src/main/java/com/contoso/hello/HelloController.java
+++ src/hello/src/main/java/com/contoso/hello/HelloController.java
@@ -6,7 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @RequestMapping("/")
+    public String root() {
+        return "Ok";
+    }
+    @RequestMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+    @RequestMapping("/health")
+    public String health() {
+        return "Ok";
     }
 }
